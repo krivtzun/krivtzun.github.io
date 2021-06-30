497881e82c8fcb9c92041375afc5518d590ce9ac
Kryvtsun Ostap <krivtzun@gmail.com>
2021-06-30 18:03:16 +0300

    food adaptive

--- Food/js/script.js
+++ Food/js/script.js
@@ -8,7 +8,7 @@ import forms from './modules/forms';
 import {openModal} from './modules/modal';
 window.addEventListener('DOMContentLoaded', () => { 
     
-    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 30000);
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 3000);
 
     tabs('.tabheader__item','.tabcontent', '.tabheader__items', 'tabheader__item_active');
     calculator();
