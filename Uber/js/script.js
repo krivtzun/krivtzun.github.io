1f7402d9a92f294f70a7c2cf9b3b63311f3dbc1d
Ostap Kryvtsun <krivtzun@gmail.com>
2021-08-05 23:42:27 +0300

    upode_test

--- Uber/js/script.js
+++ Uber/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.menu__list-item'),
+    hamburger = document.querySelector('.header__hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
